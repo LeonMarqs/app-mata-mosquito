7dfed2eaeac6fec324af3723afd190b26e825d23
Leonardo Marques <leomarques301@gmail.com>
2020-04-11 17:20:25 -0300

    desenvolvimento do app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var heigh;
 var width;
+var lifes = 1;
 
 function ajustSizeGame() {
   height = window.innerHeight;
@@ -12,7 +13,21 @@ ajustSizeGame();
 
 
 function randomPosition() {
-  // Spawn dos mosquitos
+  // Remover mosquito anterior (caso exista)
+  if (document.getElementById('fly')) {
+    document.getElementById('fly').remove();
+
+    if (lifes > 3) {
+      window.location.href = 'gameover.html';
+
+    } else {
+      
+      document.getElementById('l' + lifes).src = 'img/coracao_vazio.png';
+      lifes++;
+    }
+  }
+
+  // Posição de spawn dos mosquitos
   var xposition = Math.floor(Math.random() * width) - 90;
   var yposition = Math.floor(Math.random() * height) - 90;
 
@@ -25,17 +40,21 @@ function randomPosition() {
   // Criação de um novo elemento HTML através do DOM
   const fly = document.createElement('img');
   fly.src = 'img/mosquito.png';
-  fly.className = randomSize();
+  fly.className = randomSize() + ' ' + randomSide();
   fly.style.position = 'absolute';
   fly.style.left = xposition + 'px';
   fly.style.top = yposition + 'px';
+  fly.id = 'fly';
+  fly.onclick = function() {
+    this.remove();
+  };
 
   document.body.appendChild(fly);
+
 };
 
 function randomSize() {
   let classe = Math.floor(Math.random() * 3); //return: 0 || 1 || 2 
-  console.log(classe);
   
   switch(classe) {
     case 0:
@@ -47,3 +66,14 @@ function randomSize() {
   };
   
 };
+
+function randomSide() {
+  let classe = Math.floor(Math.random() * 2); //return: 0 || 1 || 2 
+
+  switch(classe) {
+    case 0:
+      return 'sideA';
+    case 1:
+      return 'sideB';
+  };
+}
